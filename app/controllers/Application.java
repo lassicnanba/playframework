2787ae7bb62e2faee09bb4de90037b3b69d1a273
Nanba <y.nannba@gmail.com>
2015-11-30 00:47:14 +0900

    finder追加

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,38 @@
 package controllers;
 
+import java.util.*;
+import models.*;
+
 import play.*;
+import play.data.*;
+import static play.data.Form.*;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+  // Form用の内部クラス
+  public static class SampleForm {
+    public String message;
+  }
+
+  // ルートにアクセスした際のAction
+  public static Result index() {
+    List<Message> datas = Message.find.all();
+    return ok(index.render("データベースのサンプル",datas));
+  }
+
+    // // /sendにアクセスした際のAction
+    // public static Result send(){
+    //   Form<SampleForm> f = form(SampleForm.class).bindFromRequest();
+    //   if (!f.hasErrors()){
+    //     SampleForm data = f.get();
+    //     String msg = "you typed: " + data.message;
+    //     return ok(index.render(msg,f));
+    //   } else {
+    //     return badRequest(index.render("ERROR", form(SampleForm.class)));
+    //   }
+    // }
 
 }
