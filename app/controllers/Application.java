833cea6c4d9e57f92a24e8c5ff18c76ca0f4a333
Nanba <y.nannba@gmail.com>
2015-12-10 01:13:07 +0900

    viewの追加により一覧画面(list)が表示サれるように変更

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,27 +11,17 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-
-  // Form用の内部クラス
-  public static class SampleForm {
-    public String message;
-  }
-
   // ルートにアクセスした際のAction
   public static Result index() {
-    return ok(index.render("何か書いて。", new Form(SampleForm.class)));
+    return ok(index.render(
+      "ここで渡した値がindex.scala.htmlへ渡される"
+    ));
   }
+  
+  public static Result list(){
+    return ok(list.render(
 
-    // /sendにアクセスした際のAction
-    public static Result send(){
-      Form<SampleForm> f = form(SampleForm.class).bindFromRequest();
-      if (!f.hasErrors()){
-        SampleForm data = f.get();
-        String msg = "you typed: " + data.message;
-        return ok(index.render(msg,f));
-      } else {
-        return badRequest(index.render("ERROR", form(SampleForm.class)));
-      }
-    }
+    ));
+  }
 
 }
