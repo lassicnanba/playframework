9654c5224a7b8a7d34ce715394a06dde0e8d2129
Nanba <y.nannba@gmail.com>
2015-12-12 11:19:44 +0900

    formの追加

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,13 +11,26 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
+  // Form用の内部クラス
+  public static class SampleForm {
+    public String message;
+  }
   // ルートにアクセスした際のAction
   public static Result index() {
-    return ok(index.render(
-      "ここで渡した値がindex.scala.htmlへ渡される"
-    ));
+    return ok(index.render("何か書いて",new Form(SampleForm.class)));
   }
-  
+  // /sendにアクセスした際のAction
+  public static Result send() {
+    Form<SampleForm> f = form(SampleForm.class).bindFromRequest();
+    if (!f.hasErrors()){
+      SampleForm data = f.get();
+      String msg = "you typed:" + data.message;
+      return ok(index.render(msg,f));
+    } else {
+      return badRequest(index.render("ERROR",form(SampleForm.class)));
+    }
+  }
+
   public static Result list(){
     return ok(list.render(
 
